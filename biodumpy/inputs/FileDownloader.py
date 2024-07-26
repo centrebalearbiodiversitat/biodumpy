dc3422f5cdb7fb1d2547fce0003ffc28f81ab03d
ToGo347 <ToGo347@users.noreply.github.com>
2024-07-26 08:36:11 +0000

    Auto-commit ruff style fixes

--- biodumpy/inputs/FileDownloader.py
+++ biodumpy/inputs/FileDownloader.py
@@ -3,11 +3,11 @@ import requests
 
 
 class FileDownloader(Input):
-	def __init__(self, output_format='pdf'):
+	def __init__(self, output_format="pdf"):
 		super().__init__(output_format=output_format, bulk=False)
 
 	def download(self, query, url, **kwargs):
 		try:
 			return requests.get(url, timeout=20).content
 		except:
-			return b''
+			return b""
